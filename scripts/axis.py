f02623361f2345bd5644c0c9d1b1edc83a45367b
Chris Radek <chris@timeguy.com>
2005-09-18 17:38:45 +0000

    wait until nothing is happening before switching modes.  This fixes the offset bug and the first-jog-key-is-sometimes-ignored bug.

--- scripts/axis.py
+++ scripts/axis.py
@@ -625,7 +625,9 @@ def ensure_mode(m):
     s.poll()
     if s.task_mode == m: return True
     if running(do_poll=False): return False
+    c.wait_complete()
     c.mode(m)
+    c.wait_complete()
     return True
 
 class AxisCanon(GLCanon):
@@ -1018,9 +1020,7 @@ class TclCommands(nf.TclCommands):
         s.poll()
         actual_position = s.actual_position[offset_axis]
         offset_command = "g10 L2 p1 %c%9.4f\n" % (vars.current_axis.get(), actual_position)
-        time.sleep(.1)
         c.mdi(offset_command)
-        time.sleep(.1)
         ensure_mode(emc.MODE_MANUAL)
         s.poll()
         o.tkRedraw()
