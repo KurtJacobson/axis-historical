91d46851cc638d0fb2fa9d345b5f6ccf338c4c2a
Jeff Epler <jepler@unpythonic.net>
2005-12-18 16:39:54 +0000

    cradek has checked in changes to these files, so note him as an author

--- setup/emc_setup.py
+++ setup/emc_setup.py
@@ -1,5 +1,6 @@
 #    This is a component of AXIS, a front-end for emc
 #    Copyright 2004, 2005 Jeff Epler <jepler@unpythonic.net>
+#                         and Chris Radek <chris@timeguy.com>
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
