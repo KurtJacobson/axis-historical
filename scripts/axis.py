cd7b4532276e9115f8fcc465a3d783862ec4dc79
Jeff Epler <jepler@unpythonic.net>
2006-03-20 13:43:01 +0000

    bump version number for release

--- scripts/axis.py
+++ scripts/axis.py
@@ -21,7 +21,7 @@
 from __future__ import generators
 
 import gettext; gettext.install("axis", unicode=True)
-version="1.2rc3"
+version="1.2"
 
 import sys, array, time, atexit, tempfile, shutil, os, errno
 
