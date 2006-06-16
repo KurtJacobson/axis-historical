66ea625140f690069485beec01fd04965746ce51
Jeff Epler <jepler@unpythonic.net>
2006-06-16 15:05:15 +0000

    use fewer segments on arcs

--- rs274/interpret.py
+++ rs274/interpret.py
@@ -62,7 +62,7 @@ class ArcsToSegmentsMixin:
         def interp(low, high):
             return low + (high-low) * i / steps
 
-        steps = max(8, int(256 * abs(theta1 - theta2) / math.pi))
+        steps = max(8, int(128 * abs(theta1 - theta2) / math.pi))
         p = [0] * 6
         for i in range(1, steps):
             theta = interp(theta1, theta2)
