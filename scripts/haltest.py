9cf5aa2d4ba2b51d92cf31c3d69dc484b1e0680c
Jeff Epler <jepler@unpythonic.net>
2006-07-08 22:55:09 +0000

    use several different pin types to show they work

--- scripts/haltest.py
+++ scripts/haltest.py
@@ -1,6 +1,10 @@
-import hal, time
-h = hal.component("test")
-h.newpin("i", hal.HAL_S32, hal.HAL_RD)
+from hal import *; import time
+h = component("test");
+h.newpin("i", HAL_S32, HAL_RD);
+h.newpin("f", HAL_FLOAT, HAL_RD);
+h.newpin("u8", HAL_U8, HAL_RD);
 while 1:
-    time.sleep(.2)
-    h.i = h.i + 1
+    h.i += 1;
+    h.f = h.i * .3125
+    h.u8 = (7*h.i) % 256
+    time.sleep(.1)
