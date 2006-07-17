c5710e71a8e085f8c47eac222b7a8fa0b646b748
Jeff Epler <jepler@unpythonic.net>
2006-07-17 16:44:10 +0000

    try something else for nvidia cards

--- extensions/minigl.c
+++ extensions/minigl.c
@@ -17,8 +17,8 @@
 //    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include <Python.h>
+#define GL_GLEXT_PROTOTYPES
 #include <GL/gl.h>
-#include <GL/glext.h>
 #include <GL/glu.h>
 
 static PyObject *glerror;
